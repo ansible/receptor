9de664bce541056ff2b3c4d37da11abe9bde8774
Graham Mainwaring <graham@mhn.org>
2020-06-05 22:31:25 -0400

    Add setting of IP address and route for IP tunnel

--- pkg/services/tun_proxy.go
+++ pkg/services/tun_proxy.go
@@ -3,10 +3,12 @@
 package services
 
 import (
+	"fmt"
 	"github.com/ghjm/sockceptor/pkg/cmdline"
 	"github.com/ghjm/sockceptor/pkg/debug"
 	"github.com/ghjm/sockceptor/pkg/netceptor"
 	"github.com/songgao/water"
+	"github.com/vishvananda/netlink"
 )
 
 func runTunToNetceptor(tunif *water.Interface, nconn *netceptor.PacketConn, remoteAddr netceptor.Addr) {
@@ -51,7 +53,7 @@ func runNetceptorToTun(nconn *netceptor.PacketConn, tunif *water.Interface, remo
 
 // TunProxyService runs the Receptor to tun interface proxy.
 func TunProxyService(s *netceptor.Netceptor, tunInterface string, lservice string,
-	node string, rservice string) {
+	node string, rservice string, ifaddress string, route string) {
 
 	cfg := water.Config{
 		DeviceType: water.TUN,
@@ -63,12 +65,53 @@ func TunProxyService(s *netceptor.Netceptor, tunInterface string, lservice strin
 		return
 	}
 
+	if ifaddress != "" {
+		link, err := netlink.LinkByName(iface.Name())
+		if err != nil {
+			debug.Printf("Error accessing link for tun device: %s\n", err)
+			return
+		}
+		ip, err := netlink.ParseAddr(ifaddress)
+		if err != nil {
+			debug.Printf("Error parsing IP address: %s\n", err)
+			return
+		}
+		err = netlink.AddrAdd(link, ip)
+		if err != nil {
+			debug.Printf("Error adding IP address to link: %s\n", err)
+			return
+		}
+		err = netlink.LinkSetUp(link)
+		if err != nil {
+			debug.Printf("Error setting link up: %s\n", err)
+			return
+		}
+		if route != "" {
+			ipnet, err := netlink.ParseIPNet(route)
+			if err != nil {
+				debug.Printf("Error parsing route address: %s\n", err)
+				return
+			}
+			err = netlink.RouteAdd(&netlink.Route{
+				LinkIndex: link.Attrs().Index,
+				Scope:     netlink.SCOPE_UNIVERSE,
+				Dst:       ipnet,
+			})
+			if err != nil {
+				debug.Printf("Error adding route to interface: %s\n", err)
+				return
+			}
+		}
+	}
+
 	debug.Printf("Connecting to remote netceptor node %s service %s\n", node, rservice)
 	nconn, err := s.ListenPacketAndAdvertise(lservice, map[string]string{
 		"type":          "Tunnel Proxy",
 		"interface":     tunInterface,
 		"remotenode":    node,
 		"remoteservice": rservice,
+		"ifaddress":     ifaddress,
+		"route":         route,
 	})
 
 	if err != nil {
@@ -82,16 +125,27 @@ func TunProxyService(s *netceptor.Netceptor, tunInterface string, lservice strin
 
 // TunProxyCfg is the cmdline configuration object for a tun proxy
 type TunProxyCfg struct {
-	Interface     string `required:"true" description:"Name of the tun interface"`
 	Service       string `required:"true" description:"Local Receptor service name to bind to"`
 	RemoteNode    string `required:"true" description:"Receptor node to connect to"`
 	RemoteService string `required:"true" description:"Receptor service name to connect to"`
+	Interface     string `description:"Name of the tun interface"`
+	IfAddress     string `description:"IP address to assign to the created interface"`
+	Route         string `description:"CIDR subnet to route over the created interface"`
+}
+
+// Prepare verifies we are ready to run
+func (cfg TunProxyCfg) Prepare() error {
+	if cfg.Route != "" && cfg.IfAddress == "" {
+		return fmt.Errorf("when supplying a route, an IP address must also be given")
+	}
+	return nil
 }
 
 // Run runs the action
 func (cfg TunProxyCfg) Run() error {
 	debug.Printf("Running tun proxy service %s\n", cfg)
-	go TunProxyService(netceptor.MainInstance, cfg.Interface, cfg.Service, cfg.RemoteNode, cfg.RemoteService)
+	go TunProxyService(netceptor.MainInstance, cfg.Interface, cfg.Service, cfg.RemoteNode, cfg.RemoteService,
+		cfg.IfAddress, cfg.Route)
 	return nil
 }
 

52261e3805f8d83810c3677ed00c37bc4f7a79aa
Graham Mainwaring <graham@mhn.org>
2020-06-10 21:37:47 -0400

    Process all data before closing

--- pkg/sockutils/bridge.go
+++ pkg/sockutils/bridge.go
@@ -21,24 +21,28 @@ func BridgeConns(c1 net.Conn, c2 net.Conn) {
 func bridgeHalf(c1 net.Conn, c2 net.Conn, wg *sync.WaitGroup) {
 	defer wg.Done()
 	buf := make([]byte, netceptor.MTU)
+	shouldClose := false
 	for {
 		n, err := c1.Read(buf)
 		if err != nil {
 			if err.Error() != "EOF" && !strings.Contains(err.Error(), "use of closed network connection") {
 				debug.Printf("Connection read error: %s\n", err)
 			}
-			_ = c2.Close()
-			return
+			shouldClose = true
 		}
 		debug.Tracef("    Forwarding data length %d from %s to %s\n", n,
 			c1.RemoteAddr().String(), c2.RemoteAddr().String())
 		wn, err := c2.Write(buf[:n])
 		if err != nil {
 			debug.Printf("Connection write error: %s\n", err)
-			return
+			shouldClose = true
 		}
 		if wn != n {
 			debug.Printf("Not all bytes written\n", err)
+			shouldClose = true
+		}
+		if shouldClose {
+			_ = c2.Close()
 			return
 		}
 	}

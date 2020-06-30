02d92050a5a3aa44e082852205ae7938afdbfb86
Graham Mainwaring <graham@mhn.org>
2020-06-30 18:12:23 -0400

    Fix path to golint

--- build/ci/entrypoint.sh
+++ build/ci/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-pre-commit run --all-files && \
+export PATH=/root/go/bin:$PATH
+echo "Running pre-commit" && \
+  pre-commit run --all-files && \
+  echo "Running Go builds" && \
   go build cmd/*.go && \
-  go build example/*.go
+  go build example/*.go && \
+  echo "All done"

af467eb320c298d32d82fa0e4310653e68384bac
Caleb Boylan <calebboylan@gmail.com>
2020-08-22 17:13:43 -0700

    Simplify the cli testing code and test negative costs for all listeners

--- tests/functional/cli/cli_test.go
+++ tests/functional/cli/cli_test.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/project-receptor/receptor/tests/functional/lib/utils"
 	"os/exec"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
 )
+
+func ConfirmListening(pid int) (bool, error) {
+	pidString := "pid=" + strconv.Itoa(pid)
+	var out bytes.Buffer
+	out = bytes.Buffer{}
+	ssCmd := exec.Command("ss", "-tulnp")
+	ssCmd.Stdout = &out
+	err := ssCmd.Run()
+	if err != nil {
+		return false, err
+	}
+	if strings.Contains(out.String(), pidString) {
+		return true, nil
+	}
+	return false, nil
+}
 
 func TestHelp(t *testing.T) {
 	t.Parallel()
@@ -20,129 +34,74 @@ func TestHelp(t *testing.T) {
 	}
 }
 
-func TestListenTCP(t *testing.T) {
+func TestListeners(t *testing.T) {
 	t.Parallel()
-	tcpPort := utils.ReserveTCPPort()
-	defer utils.FreeTCPPort(tcpPort)
-	receptorStdOut := bytes.Buffer{}
-	cmd := exec.Command("receptor", "--node", "id=test", "--tcp-listener", fmt.Sprintf("port=%d", tcpPort))
-	cmd.Stdout = &receptorStdOut
-	err := cmd.Start()
-
-	done := false
-	pidString := "pid=" + strconv.Itoa(cmd.Process.Pid)
-	var out bytes.Buffer
-	for timeout := 2 * time.Second; timeout > 0 && !done; {
-		out = bytes.Buffer{}
-		ssCmd := exec.Command("ss", "-tuanp")
-		ssCmd.Stdout = &out
-		err = ssCmd.Run()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, line := range strings.Split(out.String(), "\n") {
-			if strings.Contains(line, pidString) && strings.Contains(line, strconv.Itoa(tcpPort)) {
-				done = true
-				break
+	testTable := []struct {
+		listener string
+	}{
+		{"--tcp-listener"},
+		{"--ws-listener"},
+		{"--udp-listener"},
+	}
+	for _, data := range testTable {
+		listener := data.listener
+		t.Run(listener, func(t *testing.T) {
+			t.Parallel()
+			receptorStdOut := bytes.Buffer{}
+			cmd := exec.Command("receptor", "--node", "id=test", listener, "port=0")
+			cmd.Stdout = &receptorStdOut
+			err := cmd.Start()
+			if err != nil {
+				t.Fatal(err)
 			}
-		}
-		time.Sleep(100 * time.Millisecond)
-		timeout -= 100 * time.Millisecond
-	}
-	cmd.Process.Kill()
-	cmd.Process.Wait()
-	if done == false {
-		t.Fatalf("Timed out while waiting for TCP backend to start:\n%s", receptorStdOut.String())
-	}
-}
+			defer cmd.Process.Wait()
+			defer cmd.Process.Kill()
 
-func TestListenWS(t *testing.T) {
-	t.Parallel()
-	tcpPort := utils.ReserveTCPPort()
-	defer utils.FreeTCPPort(tcpPort)
-	receptorStdOut := bytes.Buffer{}
-	cmd := exec.Command("receptor", "--node", "id=test", "--ws-listener", fmt.Sprintf("port=%d", tcpPort))
-	cmd.Stdout = &receptorStdOut
-	err := cmd.Start()
-
-	done := false
-	pidString := "pid=" + strconv.Itoa(cmd.Process.Pid)
-	var out bytes.Buffer
-	for timeout := 2 * time.Second; timeout > 0 && !done; {
-		out = bytes.Buffer{}
-		ssCmd := exec.Command("ss", "-tuanp")
-		ssCmd.Stdout = &out
-		err = ssCmd.Run()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, line := range strings.Split(out.String(), "\n") {
-			if strings.Contains(line, pidString) && strings.Contains(line, strconv.Itoa(tcpPort)) {
-				done = true
-				break
+			for timeout := 2 * time.Second; timeout > 0; {
+				listening, err := ConfirmListening(cmd.Process.Pid)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if listening {
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+				timeout -= 100 * time.Millisecond
 			}
-		}
-		time.Sleep(100 * time.Millisecond)
-		timeout -= 100 * time.Millisecond
-	}
-	cmd.Process.Kill()
-	cmd.Process.Wait()
-	if done == false {
-		t.Fatalf("Timed out while waiting for WS backend to start:\n%s", receptorStdOut.String())
-	}
-}
-
-func TestListenUDP(t *testing.T) {
-	t.Parallel()
-	udpPort := utils.ReserveUDPPort()
-	defer utils.FreeUDPPort(udpPort)
-	receptorStdOut := bytes.Buffer{}
-	cmd := exec.Command("receptor", "--node", "id=test", "--udp-listener", fmt.Sprintf("port=%d", udpPort))
-	cmd.Stdout = &receptorStdOut
-	err := cmd.Start()
-
-	done := false
-	pidString := "pid=" + strconv.Itoa(cmd.Process.Pid)
-	var out bytes.Buffer
-	for timeout := 2 * time.Second; timeout > 0 && !done; {
-		out = bytes.Buffer{}
-		ssCmd := exec.Command("ss", "-tuanp")
-		ssCmd.Stdout = &out
-		err = ssCmd.Run()
-		if err != nil {
-			t.Fatal(err)
-		}
-		for _, line := range strings.Split(out.String(), "\n") {
-			if strings.Contains(line, pidString) && strings.Contains(line, strconv.Itoa(udpPort)) {
-				done = true
-				break
-			}
-		}
-		time.Sleep(100 * time.Millisecond)
-		timeout -= 100 * time.Millisecond
-	}
-	cmd.Process.Kill()
-	cmd.Process.Wait()
-	if done == false {
-		t.Fatalf("Timed out while waiting for UDP backend to start:\n%s", receptorStdOut.String())
+			t.Fatalf("Timed out while waiting for backend to start:\n%s", receptorStdOut.String())
+		})
 	}
 }
 
 func TestNegativeCost(t *testing.T) {
 	t.Parallel()
-	tcpPort := utils.ReserveTCPPort()
-	defer utils.FreeTCPPort(tcpPort)
-	receptorStdOut := bytes.Buffer{}
-	cmd := exec.Command("receptor", "--node", "id=test", "--tcp-listener", fmt.Sprintf("port=%d", tcpPort), "cost=-1")
-	cmd.Stdout = &receptorStdOut
-	err := cmd.Start()
-	if err != nil {
-		t.Fatal(err)
+	testTable := []struct {
+		listener string
+	}{
+		{"--tcp-listener"},
+		{"--ws-listener"},
+		{"--udp-listener"},
 	}
+	for _, data := range testTable {
+		listener := data.listener
+		t.Run(listener, func(t *testing.T) {
+			t.Parallel()
+			receptorStdOut := bytes.Buffer{}
+			cmd := exec.Command("receptor", "--node", "id=test", listener, "port=0", "cost=-1")
+			cmd.Stdout = &receptorStdOut
+			err := cmd.Start()
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	cmd.Process.Kill()
-	cmd.Process.Wait()
-	if cmd.ProcessState.ExitCode() == 0 {
-		t.Fatalf("receptor exited with exitcode: 0\nreceptor Stdout:\n%s", receptorStdOut.String())
+			// Wait for our process to hopefully run and quit
+			time.Sleep(100 * time.Millisecond)
+
+			cmd.Process.Kill()
+			cmd.Process.Wait()
+			if receptorStdOut.String() != "Error: connection cost must be positive\n" {
+				t.Fatalf("Expected stdout: Error: connection cost must be positive, actual stdout: %s", receptorStdOut.String())
+			}
+		})
 	}
 }

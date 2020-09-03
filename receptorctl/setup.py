f3b26e4c324dd4795b54c54dda90d91d30630d97
Graham Mainwaring <graham@mhn.org>
2020-09-02 20:53:56 -0400

    Initial RPM packaging

--- receptorctl/setup.py
+++ receptorctl/setup.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
+import json
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+verfile = None
+for fn in ['VERSION', '../VERSION']:
+    if os.path.exists(fn):
+        verfile = fn
+        break
+if verfile is None:
+    raise IOError("Version file not found.")
+with open(verfile, 'r') as f:
+    verinfo = json.load(f)
+
 setup(
     name="receptorctl",
-    version="0.0.1",
-    author='Red Hat Ansible',
+    version=verinfo['version'],
+    author='Red Hat',
     url="https://github.com/project-receptor/receptor/receptorctl",
-    license='Apache',
+    license='APL 2.0',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',

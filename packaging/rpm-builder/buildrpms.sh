143fc45bb7c5e9a988df87d09d4cd3fde372d555
Graham Mainwaring <graham@mhn.org>
2020-09-06 01:11:19 -0400

    Add packaging for receptor-plugin-worker

--- packaging/rpm-builder/buildrpms.sh
+++ packaging/rpm-builder/buildrpms.sh
@@ -5,4 +5,5 @@ mkdir -p $PWD/rpmbuild/SOURCES
 git archive HEAD --format=tar.gz --prefix=receptor-$VERSION/ > $PWD/rpmbuild/SOURCES/receptor-$VERSION.tar.gz
 rpmbuild -ba packaging/rpm/receptor.spec
 rpmbuild -ba packaging/rpm/receptorctl.spec
+rpmbuild -ba packaging/rpm/receptor-python-worker.spec
 createrepo $PWD/rpmbuild/RPMS

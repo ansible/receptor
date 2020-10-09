65740603e87e9d5c33a147f0cae0face87ae4f01
Graham Mainwaring <graham@mhn.org>
2020-10-09 00:43:10 -0400

    Update RPM building to work on non-Fedora platforms

--- packaging/rpm-builder/buildrpms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e  # exit on any error
-VERSION=$(cat $PWD/.VERSION)
-mkdir -p $PWD/rpmbuild/SOURCES
-git archive HEAD --format=tar.gz --prefix=receptor-$VERSION/ > $PWD/rpmbuild/SOURCES/receptor-$VERSION.tar.gz
-rpmbuild -ba packaging/rpm/receptor.spec
-rpmbuild -ba packaging/rpm/receptorctl.spec
-rpmbuild -ba packaging/rpm/receptor-python-worker.spec
-createrepo $PWD/rpmbuild/RPMS

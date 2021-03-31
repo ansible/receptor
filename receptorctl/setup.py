399cd9cf14ce7cd07553a4c306990810ac489df3
Shane McDonald <me@shanemcd.com>
2021-03-31 12:58:18 -0400

    Use PBR for receptorctl
    Removes the need for creating / reading the .VERSION file

--- receptorctl/setup.py
+++ receptorctl/setup.py
@@ -1,50 +1,8 @@
 #!/usr/bin/env python
 
-import os
-import subprocess
-from setuptools import setup, find_packages
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
-verfile = None
-for fn in ['.VERSION', '../.VERSION']:
-    if os.path.exists(fn):
-        verfile = fn
-        break
-
-if verfile is None:
-    subprocess.run(["make", "version"], cwd="../")
-    verfile = '../.VERSION'
-
-    if not os.path.exists(verfile):
-        raise IOError("Version file not found.")
-
-with open(verfile, 'r') as f:
-    version = f.readline().rstrip('\n\r')
+from setuptools import setup
 
 setup(
-    name="receptorctl",
-    version=version,
-    author='Red Hat',
-    url="https://github.com/project-receptor/receptor/receptorctl",
-    license='APL 2.0',
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "setuptools",
-        "python-dateutil",
-        "click",
-        "pyyaml"
-    ],
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-              'receptorctl=receptorctl:run'
-          ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
+    setup_requires=['pbr'],
+    pbr=True,
 )

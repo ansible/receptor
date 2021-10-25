4ee297560cf2cc9c2546bf455c9f02ef321415a5
Tiago <tiago.goes2009@gmail.com>
2021-10-25 18:28:59 -0300

    CLI tests for https://github.com/ansible/receptor/issues/431

--- .history/receptorctl/tests/test_cli_20211122111952.py
+++ .history/receptorctl/tests/test_cli_20211122111952.py
@@ -3,8 +3,7 @@ from receptorctl import cli as commands
 # The goal is to write tests following the click documentation:
 # https://click.palletsprojects.com/en/8.0.x/testing/
 
-import pytest
-import json
+import pytest, json
 
 
 @pytest.mark.usefixtures("receptor_mesh_mesh1")

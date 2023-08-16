f6d1cf8b21b1b165bad711d529f01b39cf2ed0d9
matoval <mat24c@gmail.com>
2023-08-15 19:12:44 -0700

    Fix & clean up tests

--- pkg/netceptor/ping_test.go
+++ pkg/netceptor/ping_test.go
@@ -21,7 +21,46 @@ func setupTest(t *testing.T) (*gomock.Controller, *mock_netceptor.MockNetceptorF
 	// Now you can call Ping and it will use your mock Netceptor and PacketConn
 	ctx := context.Background()
 
+	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any()).AnyTimes()
+	mockPacketConn.EXPECT().Close().Return(nil).AnyTimes()
+	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{}).AnyTimes()
+
 	return ctrl, mockNetceptor, mockPacketConn, ctx
+}
+
+func TestListenSubscribeUnreachableErr(t *testing.T) {
+	ctrl, mockNetceptor, mockPacketConn, ctx := setupTest(t)
+	defer ctrl.Finish()
+	defer ctx.Done()
+
+	mockUnreachableMessage := netceptor.UnreachableMessage{
+		FromNode:    "",
+		ToNode:      "",
+		FromService: "",
+		ToService:   "",
+		Problem:     "test",
+	}
+
+	mockUnreachableNotification := netceptor.UnreachableNotification{
+		mockUnreachableMessage,
+		"test",
+	}
+
+	channel := make(chan netceptor.UnreachableNotification)
+
+	// Set up the mock behaviours
+	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
+	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
+	mockNetceptor.EXPECT().Context().Return(context.Background()).MaxTimes(2)
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, nil, errors.New("error")).Times(1)
+	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(channel)
+	go func() {
+		channel <- mockUnreachableNotification
+	}()
+	_, _, subscribeUnreachableError := netceptor.CreatePing(ctx, mockNetceptor, "target", 1)
+	if subscribeUnreachableError == nil {
+		t.Fatal("SubscribeUnreachable expected to return error but returned nil")
+	}
 }
 
 func TestCreatePing(t *testing.T) {
@@ -31,12 +70,9 @@ func TestCreatePing(t *testing.T) {
 
 	// Set up the mock behaviours
 	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
-	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any())
 	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(make(chan netceptor.UnreachableNotification))
-	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, &netceptor.Addr{}, nil)
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, &netceptor.Addr{}, nil).Times(1)
 	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
-	mockPacketConn.EXPECT().Close().Return(nil)
-	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{})
 	mockNetceptor.EXPECT().Context().Return(context.Background()).Times(2)
 
 	// dur, nodeID, err := mockNetceptor.Ping(ctx, "target", 1)
@@ -70,45 +106,6 @@ func TestListenPacketErr(t *testing.T) {
 	}
 }
 
-func TestListenSubscribeUnreachableErr(t *testing.T) {
-	ctrl, mockNetceptor, mockPacketConn, ctx := setupTest(t)
-	defer ctrl.Finish()
-	defer ctx.Done()
-
-	mockUnreachableMessage := netceptor.UnreachableMessage{
-		FromNode:    "",
-		ToNode:      "",
-		FromService: "",
-		ToService:   "",
-		Problem:     "test",
-	}
-
-	mockUnreachableNotification := netceptor.UnreachableNotification{
-		mockUnreachableMessage,
-		"test",
-	}
-
-	channel := make(chan netceptor.UnreachableNotification)
-
-	// Set up the mock behaviours
-	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
-	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any())
-	mockPacketConn.EXPECT().Close().Return(nil)
-	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{})
-	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
-	mockNetceptor.EXPECT().Context().Return(context.Background()).Times(1)
-	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, &netceptor.Addr{}, nil).AnyTimes()
-	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(channel)
-	go func() {
-		channel <- mockUnreachableNotification
-	}()
-	time.Sleep(time.Second * 2)
-	_, _, subscribeUnreachableError := netceptor.CreatePing(ctx, mockNetceptor, "target", 1)
-	if subscribeUnreachableError == nil {
-		t.Fatal("SubscribeUnreachable expected to return error but returned nil")
-	}
-}
-
 func TestReadFromErr(t *testing.T) {
 	ctrl, mockNetceptor, mockPacketConn, ctx := setupTest(t)
 	defer ctrl.Finish()
@@ -116,13 +113,10 @@ func TestReadFromErr(t *testing.T) {
 
 	// Set up the mock behaviours
 	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
-	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any())
-	mockPacketConn.EXPECT().Close().Return(nil)
-	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{})
 	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
 	mockNetceptor.EXPECT().Context().Return(context.Background()).Times(2)
 	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(make(chan netceptor.UnreachableNotification))
-	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, &netceptor.Addr{}, errors.New("ReadFrom error"))
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, nil, errors.New("ReadFrom error")).Times(1)
 
 	_, _, readFromError := netceptor.CreatePing(ctx, mockNetceptor, "target", 1)
 	if readFromError == nil {
@@ -137,11 +131,10 @@ func TestWriteToErr(t *testing.T) {
 
 	// Set up the mock behaviours
 	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
-	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any())
-	mockPacketConn.EXPECT().Close().Return(nil)
-	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{})
 	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(make(chan netceptor.UnreachableNotification))
 	mockNetceptor.EXPECT().NodeID().Return("nodeID")
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, nil, errors.New("ReadFrom error")).MaxTimes(1)
+	mockNetceptor.EXPECT().Context().Return(context.Background()).MaxTimes(2)
 	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, errors.New("WriteTo error"))
 
 	_, _, writeToError := netceptor.CreatePing(ctx, mockNetceptor, "target", 1)
@@ -157,20 +150,40 @@ func TestTimeOutErr(t *testing.T) {
 
 	// Set up the mock behaviours
 	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
-	mockPacketConn.EXPECT().SetHopsToLive(gomock.Any())
 	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(make(chan netceptor.UnreachableNotification))
-	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).DoAndReturn(func(arg []uint8) (int, *netceptor.Addr, error) {
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Do(func([]byte) {
 		time.Sleep(time.Second * 11)
-		return 0, &netceptor.Addr{}, nil
-	})
+	}).Times(1)
 	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
-	mockPacketConn.EXPECT().Close().Return(nil)
-	mockNetceptor.EXPECT().NewAddr(gomock.Any(), gomock.Any()).Return(netceptor.Addr{})
-	mockNetceptor.EXPECT().Context().Return(context.Background()).Times(1)
+	mockNetceptor.EXPECT().Context().Return(context.Background()).MaxTimes(2)
 
 	_, _, err := netceptor.CreatePing(ctx, mockNetceptor, "target", 1)
 	if err.Error() != "timeout" {
-		t.Fatalf("Unexpected error: %v", err)
+		t.Fatalf("Expected error to be 'timeout' but got %v", err)
 	}
+}
 
+func TestUserCancel(t *testing.T) {
+	ctrl, mockNetceptor, mockPacketConn, ctx := setupTest(t)
+	defer ctrl.Finish()
+	defer ctx.Done()
+
+	// Set up the mock behaviours
+	mockNetceptor.EXPECT().ListenPacket(gomock.Any()).Return(mockPacketConn, nil)
+	mockPacketConn.EXPECT().SubscribeUnreachable(gomock.Any()).Return(make(chan netceptor.UnreachableNotification))
+	mockPacketConn.EXPECT().ReadFrom(gomock.Any()).Return(0, &netceptor.Addr{}, nil).Times(1)
+	mockPacketConn.EXPECT().WriteTo(gomock.Any(), gomock.Any()).Return(0, nil)
+	mockNetceptor.EXPECT().Context().DoAndReturn(func() context.Context {
+		time.Sleep(time.Second * 1)
+		return context.Background()
+	}).Times(2)
+
+	newCtx, ctxCancel := context.WithCancel(ctx)
+
+	time.AfterFunc(1*time.Second, ctxCancel)
+
+	_, _, err := netceptor.CreatePing(newCtx, mockNetceptor, "target", 1)
+	if err.Error() != "user cancelled" {
+		t.Fatalf("Expected error to be 'user cancelled' but got %v", err)
+	}
 }

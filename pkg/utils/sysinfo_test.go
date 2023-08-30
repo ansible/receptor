e86d292045cbcf70786e036c274a9b9baacf24f9
Thom Carlin <tcarlin@redhat.com>
2023-08-30 11:34:05 -0400

    AAP-14352: Add sysinfo unit test

---
+++ pkg/utils/sysinfo_test.go
@@ -0,0 +1,41 @@
+package utils_test
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/ansible/receptor/pkg/utils"	//nolint:depguard
+	"github.com/stretchr/testify/assert"	//nolint:depguard
+)
+
+func TestGetSysCPUCount(t *testing.T) {
+	got := utils.GetSysCPUCount()
+	assert.Positive(t, got)
+
+	commandOutput, err1 := exec.Command("nproc").CombinedOutput()
+
+	assert.Nilf(t, err1, "Unable to get nproc output, output = %s, err = %s", string(commandOutput), err1)
+
+	commandOutputWithout := strings.TrimSpace(string(commandOutput))
+	want, err2 := strconv.Atoi(commandOutputWithout)
+	assert.Nilf(t, err2, "Unable to convert nproc output %s, err = %s", string(commandOutput), err2)
+
+	assert.Equal(t, got, want)
+}
+
+func TestGetSysMemoryMiB(t *testing.T) {
+	got := utils.GetSysMemoryMiB()
+	assert.Positive(t, got)
+
+	commandOutput, err1 := exec.Command("sed", "-n", "s/^MemTotal:[[:space:]]*\\([[:digit:]]*\\).*/\\1/p", "/proc/meminfo").CombinedOutput()
+	assert.Nilf(t, err1, "Unable to get /proc/meminfo output, err = %s", err1)
+
+	commandOutputWithout := strings.TrimSpace(string(commandOutput))
+	wantKb, err2 := strconv.ParseUint(commandOutputWithout, 10, 64)
+	assert.Nilf(t, err2, "Unable to convert /proc/meminfo output %s, err = %s", string(commandOutput), err2)
+
+	want := wantKb / 1024
+	assert.Equal(t, got, want)
+}

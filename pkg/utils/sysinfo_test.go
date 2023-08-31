ebf087fbc5026f8e44d6b477a6e8e89dcb3cd1f4
Thom Carlin <tcarlin@redhat.com>
2023-08-31 15:30:01 -0400

    Addressed comments by @AaronH88 and @shanemcd

--- pkg/utils/sysinfo_test.go
+++ pkg/utils/sysinfo_test.go
@@ -2,6 +2,7 @@ package utils_test
 
 import (
 	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 	"testing"
@@ -14,28 +15,27 @@ func TestGetSysCPUCount(t *testing.T) {
 	got := utils.GetSysCPUCount()
 	assert.Positive(t, got)
 
-	commandOutput, err1 := exec.Command("nproc").CombinedOutput()
+	if runtime.GOOS == "linux" {
+		commandOutput, _ := exec.Command("nproc").CombinedOutput()
 
-	assert.Nilf(t, err1, "Unable to get nproc output, output = %s, err = %s", string(commandOutput), err1)
+		commandOutputWithout := strings.TrimSpace(string(commandOutput))
+		want, _ := strconv.Atoi(commandOutputWithout)
 
-	commandOutputWithout := strings.TrimSpace(string(commandOutput))
-	want, err2 := strconv.Atoi(commandOutputWithout)
-	assert.Nilf(t, err2, "Unable to convert nproc output %s, err = %s", string(commandOutput), err2)
-
-	assert.Equal(t, got, want)
+		assert.Equal(t, got, want)
+	}
 }
 
 func TestGetSysMemoryMiB(t *testing.T) {
 	got := utils.GetSysMemoryMiB()
 	assert.Positive(t, got)
 
-	commandOutput, err1 := exec.Command("sed", "-n", "s/^MemTotal:[[:space:]]*\\([[:digit:]]*\\).*/\\1/p", "/proc/meminfo").CombinedOutput()
-	assert.Nilf(t, err1, "Unable to get /proc/meminfo output, err = %s", err1)
+	if runtime.GOOS == "linux" {
+		commandOutput, _ := exec.Command("sed", "-n", "s/^MemTotal:[[:space:]]*\\([[:digit:]]*\\).*/\\1/p", "/proc/meminfo").CombinedOutput()
 
-	commandOutputWithout := strings.TrimSpace(string(commandOutput))
-	wantKb, err2 := strconv.ParseUint(commandOutputWithout, 10, 64)
-	assert.Nilf(t, err2, "Unable to convert /proc/meminfo output %s, err = %s", string(commandOutput), err2)
+		commandOutputWithout := strings.TrimSpace(string(commandOutput))
+		wantKb, _ := strconv.ParseUint(commandOutputWithout, 10, 64)
 
-	want := wantKb / 1024
-	assert.Equal(t, got, want)
+		want := wantKb / 1024
+		assert.Equal(t, got, want)
+	}
 }

3cd97e4c14dbfc158ae7161db6074eef399716a6
Thom Carlin <tcarlin@redhat.com>
2023-09-01 11:51:28 -0400

    Remove testify

--- pkg/utils/sysinfo_test.go
+++ pkg/utils/sysinfo_test.go
@@ -8,12 +8,13 @@ import (
 	"testing"
 
 	"github.com/ansible/receptor/pkg/utils"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGetSysCPUCount(t *testing.T) {
 	got := utils.GetSysCPUCount()
-	assert.Positive(t, got)
+	if ( got <= 0 ) {
+		t.Fail()
+	}
 
 	if runtime.GOOS == "linux" {
 		commandOutput, _ := exec.Command("nproc").CombinedOutput()
@@ -21,13 +22,17 @@ func TestGetSysCPUCount(t *testing.T) {
 		commandOutputWithout := strings.TrimSpace(string(commandOutput))
 		want, _ := strconv.Atoi(commandOutputWithout)
 
-		assert.Equal(t, got, want)
+		if ( got != want) {
+			t.Fail()
+		}
 	}
 }
 
 func TestGetSysMemoryMiB(t *testing.T) {
 	got := utils.GetSysMemoryMiB()
-	assert.Positive(t, got)
+	if ( got <= 0 ) {
+		t.Fail()
+	}
 
 	if runtime.GOOS == "linux" {
 		commandOutput, _ := exec.Command("sed", "-n", "s/^MemTotal:[[:space:]]*\\([[:digit:]]*\\).*/\\1/p", "/proc/meminfo").CombinedOutput()
@@ -36,6 +41,8 @@ func TestGetSysMemoryMiB(t *testing.T) {
 		wantKb, _ := strconv.ParseUint(commandOutputWithout, 10, 64)
 
 		want := wantKb / 1024
-		assert.Equal(t, got, want)
+		if ( got != want) {
+			t.Fail()
+		}
 	}
 }

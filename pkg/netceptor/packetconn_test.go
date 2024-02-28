d00237770407d51a88347cea89255c42244150a3
Thom Carlin <tcarlin@redhat.com>
2024-02-28 12:18:37 -0500

    Mark unused-parameter issues

--- pkg/netceptor/packetconn_test.go
+++ pkg/netceptor/packetconn_test.go
@@ -155,7 +155,7 @@ func TestPacketConn(t *testing.T) {
 		{
 			"ReadFrom Error",
 			"",
-			func(ctx context.Context) {
+			func(ctx context.Context) { //nolint:revive
 				newCtx, ctxCancel := context.WithCancel(context.Background())
 				time.AfterFunc(time.Microsecond*200, ctxCancel)
 				mockNetceptorForPacketConn.EXPECT().Context().Return(newCtx)

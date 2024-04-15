385a59d0a74581faf8cadd0ff9c17cf093ed4d0b
Don Naro <dnaro@redhat.com>
2024-04-15 16:33:26 +0100

    add pip-compile nox session

--- receptorctl/noxfile.py
+++ receptorctl/noxfile.py
@@ -1,6 +1,7 @@
-from glob import iglob
-
 import os
+from glob import iglob
+from pathlib import Path
+
 import nox
 
 python_versions = ["3.8", "3.9", "3.10", "3.11"]
@@ -11,12 +12,20 @@ LINT_FILES: tuple[str, ...] = (
 )
 PINNED = os.environ.get("PINNED", "true").lower() in {"1", "true"}
 
+requirements_directory = Path("requirements").resolve()
+
+requirements_files = [
+    requirements_input_file_path.stem
+    for requirements_input_file_path in requirements_directory.glob("*.in")
+]
+
+
 def install(session: nox.Session, *args, req: str, **kwargs):
     if PINNED:
-        pip_constraint = f"requirements/{req}.txt"
+        pip_constraint = requirements_directory / f"{req}.txt"
         kwargs.setdefault("env", {})["PIP_CONSTRAINT"] = pip_constraint
         session.log(f"export PIP_CONSTRAINT={pip_constraint!r}")
-    session.install("-r", f"requirements/{req}.in", *args, **kwargs)
+    session.install("-r", requirements_directory / f"{req}.in", *args, **kwargs)
 
 
 @nox.session(python=python_versions)
@@ -63,3 +72,34 @@ def lint(session: nox.Session):
     """
     session.notify("check_style")
     session.notify("check_format")
+
+
+@nox.session(name="pip-compile", python=["3.12"])
+@nox.parametrize(["req"], arg_values_list=requirements_files, ids=requirements_files)
+def pip_compile(session: nox.Session, req: str):
+    """Generate lock files from input files or upgrade packages in lock files."""
+    # fmt: off
+    session.install(
+      "-r", str(requirements_directory / "pip-tools.in"),
+      "-c", str(requirements_directory / "pip-tools.txt"),
+    )
+    # fmt: on
+
+    # Use --upgrade by default unless a user passes -P.
+    upgrade_related_cli_flags = ("-P", "--upgrade-package", "--no-upgrade")
+    has_upgrade_related_cli_flags = any(
+        arg.startswith(upgrade_related_cli_flags) for arg in session.posargs
+    )
+    injected_extra_cli_args = () if has_upgrade_related_cli_flags else ("--upgrade",)
+
+    output_file = Path(f"requirements/{req}.txt")
+    input_file = Path(f"requirements/{req}.in")
+
+    session.run(
+        "pip-compile",
+        "--output-file",
+        str(output_file),
+        *session.posargs,
+        *injected_extra_cli_args,
+        str(input_file),
+    )

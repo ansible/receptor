34f7ac90b713df5416f0ca5e01d4f1c4d43dda01
Thom Carlin <tcarlin@redhat.com>
2024-08-09 11:20:42 -0400

    Change incremental_duration to use black box testing (#1117)

--- pkg/utils/incremental_duration_test.go
+++ pkg/utils/incremental_duration_test.go
@@ -1,15 +1,17 @@
-package utils
+package utils_test
 
 import (
 	"testing"
 	"time"
+
+	"github.com/ansible/receptor/pkg/utils"
 )
 
 const newIncrementalDurationMessage string = "NewIncrementalDuration() = %v, want %v"
 
 func TestNewIncrementalDuration(t *testing.T) {
 	type args struct {
-		duration    time.Duration
+		Duration    time.Duration
 		maxDuration time.Duration
 		multiplier  float64
 	}
@@ -22,7 +24,7 @@ func TestNewIncrementalDuration(t *testing.T) {
 		{
 			name: "NewIncrementalDuration1",
 			args: args{
-				duration:    1 * time.Second,
+				Duration:    1 * time.Second,
 				maxDuration: 10 * time.Second,
 				multiplier:  2.0,
 			},
@@ -31,7 +33,7 @@ func TestNewIncrementalDuration(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewIncrementalDuration(tt.args.duration, tt.args.maxDuration, tt.args.multiplier); got.duration != tt.want {
+			if got := utils.NewIncrementalDuration(tt.args.Duration, tt.args.maxDuration, tt.args.multiplier); got.Duration != tt.want {
 				t.Errorf(newIncrementalDurationMessage, got, tt.want)
 			}
 		})
@@ -39,44 +41,44 @@ func TestNewIncrementalDuration(t *testing.T) {
 }
 
 func TestIncrementalDurationReset(t *testing.T) {
-	delay := NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
+	delay := utils.NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
 	want1 := 1 * time.Second
-	if delay.duration != want1 {
-		t.Errorf(newIncrementalDurationMessage, delay.duration, want1)
+	if delay.Duration != want1 {
+		t.Errorf(newIncrementalDurationMessage, delay.Duration, want1)
 	}
 	<-delay.NextTimeout()
 
 	want2 := 2 * time.Second
-	if delay.duration != want2 {
-		t.Errorf(newIncrementalDurationMessage, delay.duration, want2)
+	if delay.Duration != want2 {
+		t.Errorf(newIncrementalDurationMessage, delay.Duration, want2)
 	}
 	delay.Reset()
-	if delay.duration != want1 {
-		t.Errorf("Reset() = %v, want %v", delay.duration, want1)
+	if delay.Duration != want1 {
+		t.Errorf("Reset() = %v, want %v", delay.Duration, want1)
 	}
 }
 
 func TestIncrementalDurationincreaseDuration(t *testing.T) {
-	delay := NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
+	delay := utils.NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
 	for i := 0; i <= 10; i++ {
-		delay.increaseDuration()
+		delay.IncreaseDuration()
 	}
 	want10 := 10 * time.Second
-	if delay.duration != want10 {
-		t.Errorf("increaseDuration() = %v, want %v", delay.duration, want10)
+	if delay.Duration != want10 {
+		t.Errorf("increaseDuration() = %v, want %v", delay.Duration, want10)
 	}
 }
 
 func TestIncrementalDurationNextTimeout(t *testing.T) {
-	delay := NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
+	delay := utils.NewIncrementalDuration(1*time.Second, 10*time.Second, 2.0)
 	want1 := 1 * time.Second
-	if delay.duration != want1 {
-		t.Errorf(newIncrementalDurationMessage, delay.duration, want1)
+	if delay.Duration != want1 {
+		t.Errorf(newIncrementalDurationMessage, delay.Duration, want1)
 	}
 	<-delay.NextTimeout()
 
 	want2 := 2 * time.Second
-	if delay.duration != want2 {
-		t.Errorf("NextTimeout() = %v, want %v", delay.duration, want2)
+	if delay.Duration != want2 {
+		t.Errorf("NextTimeout() = %v, want %v", delay.Duration, want2)
 	}
 }

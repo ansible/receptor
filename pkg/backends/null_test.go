86a8fa5441b4f0d91db9c22882f2f12384b995c6
Thom Carlin <tcarlin@redhat.com>
2024-09-05 15:19:13 -0400

    Add gettls null backend test

--- pkg/backends/null_test.go
+++ pkg/backends/null_test.go
@@ -1,6 +1,12 @@
-package backends
+package backends_test
 
-import "testing"
+import (
+	"crypto/tls"
+	"reflect"
+	"testing"
+
+	"github.com/ansible/receptor/pkg/backends"
+)
 
 func TestNullBackendCfgGetAddr(t *testing.T) {
 	type fields struct {
@@ -21,11 +27,40 @@ func TestNullBackendCfgGetAddr(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cfg := &NullBackendCfg{
+			cfg := &backends.NullBackendCfg{
 				Local: tt.fields.Local,
 			}
 			if got := cfg.GetAddr(); got != tt.want {
 				t.Errorf("NullBackendCfg.GetAddr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNullBackendCfgGetTLS(t *testing.T) {
+	type fields struct {
+		Local bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *tls.Config
+	}{
+		{
+			name:	"Positive",
+			fields:	fields{
+				Local:	true,
+			},
+			want:	nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := &backends.NullBackendCfg{
+				Local: tt.fields.Local,
+			}
+			if got := cfg.GetTLS(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NullBackendCfg.GetTLS() = %v, want %v", got, tt.want)
 			}
 		})
 	}

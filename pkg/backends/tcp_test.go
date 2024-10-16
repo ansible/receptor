0353185c82419a1ddeb65d794ab155a14aebbd75
Thom Carlin <tcarlin@redhat.com>
2024-10-16 12:11:53 -0400

    Remove comment block

--- pkg/backends/tcp_test.go
+++ pkg/backends/tcp_test.go
@@ -8,45 +8,6 @@ import (
 	"github.com/ansible/receptor/pkg/logger"
 )
 
-/* func TestNewTCPDialer(t *testing.T) {
-	type args struct {
-		address string
-		redial  bool
-		tls     *tls.Config
-		logger  *logger.ReceptorLogger
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *backends.TCPDialer
-		wantErr bool
-	}{
-		{
-			name: "Positive",
-			args: args{
-				address: "127.0.0.1:9999",
-				redial:  true,
-				tls:     nil,
-				logger:  nil,
-			},
-			want:    nil,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := backends.NewTCPDialer(tt.args.address, tt.args.redial, tt.args.tls, tt.args.logger)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewTCPDialer() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewTCPDialer() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-} */
-
 func TestNewTCPListener(t *testing.T) {
 	type args struct {
 		address string

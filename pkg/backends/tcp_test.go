d9a74dd0c73504ef3dd4ca37b1bcf854091e58d0
Thom Carlin <tcarlin@redhat.com>
2024-10-17 12:09:45 -0400

    Add tcp start test

--- pkg/backends/tcp_test.go
+++ pkg/backends/tcp_test.go
@@ -1,11 +1,15 @@
 package backends
 
 import (
+	"context"
 	"crypto/tls"
+	"net"
 	"reflect"
+	"sync"
 	"testing"
 
 	"github.com/ansible/receptor/pkg/logger"
+	"github.com/ansible/receptor/pkg/netceptor"
 )
 
 func TestNewTCPListener(t *testing.T) {
@@ -47,6 +51,63 @@ func TestNewTCPListener(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewTCPListener() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTCPListenerStart(t *testing.T) {
+	type fields struct {
+		address string
+		TLS     *tls.Config
+		li      net.Listener
+		innerLi *net.TCPListener
+		logger  *logger.ReceptorLogger
+	}
+	type args struct {
+		ctx context.Context
+		wg  *sync.WaitGroup
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    chan netceptor.BackendSession
+		wantErr bool
+	}{
+		{
+			name:    "Positive",
+			fields:  fields{
+				address:	"127.0.0.1:9998",
+				TLS:		nil,
+				li:			nil,
+				innerLi:	nil,
+				logger:		logger.NewReceptorLogger("TCPtest"),
+			},
+			args:    args{
+				ctx: context.Background(),
+				wg:  &sync.WaitGroup{},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &TCPListener{
+				address: tt.fields.address,
+				TLS:     tt.fields.TLS,
+				li:      tt.fields.li,
+				innerLi: tt.fields.innerLi,
+				logger:  tt.fields.logger,
+			}
+			got, err := b.Start(tt.args.ctx, tt.args.wg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TCPListener.Start() error = %+v, wantErr %+v", err, tt.wantErr)
+
+				return
+			}
+			if got == nil {
+				t.Errorf("TCPListener.Start() returned nil" )
 			}
 		})
 	}

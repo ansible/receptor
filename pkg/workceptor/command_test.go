90726b7e0684f0c3f825b994d5a940d644fa008e
matoval <mat24c@gmail.com>
2024-12-12 13:26:33 -0800

    remove parallel

--- pkg/workceptor/command_test.go
+++ pkg/workceptor/command_test.go
@@ -89,7 +89,6 @@ func TestCommandSetFromParams(t *testing.T) {
 }
 
 func TestUnredactedStatus(t *testing.T) {
-	t.Parallel()
 	wu, mockBaseWorkUnit, _, _ := createCommandTestSetup(t)
 	restartTestCases := []struct {
 		name string

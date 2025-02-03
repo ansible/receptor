25498aa8b3f2cce3c0fda577247109f10568e023
matoval <mat24c@gmail.com>
2025-02-03 12:35:49 -0800

    Revert lock

--- pkg/workceptor/kubernetes.go
+++ pkg/workceptor/kubernetes.go
@@ -1463,6 +1463,7 @@ func (kw *KubeUnit) Status() *StatusFileData {
 // Status returns a copy of the status currently loaded in memory.
 func (kw *KubeUnit) UnredactedStatus() *StatusFileData {
 	kw.GetStatusLock().RLock()
+	defer kw.GetStatusLock().RUnlock()
 	status := kw.GetStatusWithoutExtraData()
 	ked, ok := kw.GetStatusCopy().ExtraData.(*KubeExtraData)
 	if ok {

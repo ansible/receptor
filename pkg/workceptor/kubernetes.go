58d50c548dc9193e09596f7df0ac50b7a2bcbd6b
matoval <mat24c@gmail.com>
2025-02-04 20:41:51 -0800

    Replace defer unlock with explicit unlock

--- pkg/workceptor/kubernetes.go
+++ pkg/workceptor/kubernetes.go
@@ -1491,9 +1491,9 @@ func (kw *KubeUnit) Status() *StatusFileData {
 // Status returns a copy of the status currently loaded in memory.
 func (kw *KubeUnit) UnredactedStatus() *StatusFileData {
 	kw.GetStatusLock().RLock()
-	defer kw.GetStatusLock().RUnlock()
 	status := kw.GetStatusWithoutExtraData()
 	ked, ok := kw.GetStatusCopy().ExtraData.(*KubeExtraData)
+	kw.GetStatusLock().RUnlock()
 	if ok {
 		kedCopy := *ked
 		status.ExtraData = &kedCopy

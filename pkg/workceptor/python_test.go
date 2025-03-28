b137fb0f14c04c48eaea03cdf4648d0cbe9c2072
Dave Mulford <dmulford@redhat.com>
2025-03-28 09:56:56 -0400

    Fix suggested issues in python.go tests

--- pkg/workceptor/python_test.go
+++ pkg/workceptor/python_test.go
@@ -1,154 +1,52 @@
-package workceptor
+package workceptor_test
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"math"
+
 	"os"
 	"path"
 	"strings"
+
 	"sync"
 	"testing"
 
 	"slices"
 
-	"github.com/ansible/receptor/pkg/logger"
-	"github.com/ansible/receptor/pkg/netceptor"
-	// "github.com/ansible/receptor/pkg/workceptor/mock_workceptor"
+	"github.com/ansible/receptor/pkg/workceptor"
+	"github.com/ansible/receptor/pkg/workceptor/mock_workceptor"
+	"go.uber.org/mock/gomock"
 )
 
-type baseWorkUnitForWorkUnitMock struct {
-	statusLock *sync.RWMutex
-}
+const (
+	deprecationWarning = "[DEPRECATION WARNING] '--work-python' option is not currently being used. This feature will be removed from receptor in a future release."
+	launchPythonRunner = "Launching Python runner"
+)
 
-func (mock *baseWorkUnitForWorkUnitMock) CancelContext() {
+func createPythonUnitTestSetup(t *testing.T) (workceptor.WorkUnit, *mock_workceptor.MockBaseWorkUnitForWorkUnit, *mock_workceptor.MockNetceptorForWorkceptor, *workceptor.Workceptor) {
+	ctrl := gomock.NewController(t)
+	ctx := context.Background()
 
-}
+	mockBaseWorkUnit := mock_workceptor.NewMockBaseWorkUnitForWorkUnit(ctrl)
+	mockNetceptor := mock_workceptor.NewMockNetceptorForWorkceptor(ctrl)
+	mockNetceptor.EXPECT().NodeID().Return("NodeID")
+	mockNetceptor.EXPECT().GetLogger()
 
-func (mock *baseWorkUnitForWorkUnitMock) ID() string {
-	return ""
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) Init(w *Workceptor, unitID string, workType string, fs FileSystemer, watcher WatcherWrapper) {
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) LastUpdateError() error {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) Load() error {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) MonitorLocalStatus() {
-
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) Release(force bool) error {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) Save() error {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) SetFromParams(_ map[string]string) error {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) Status() *StatusFileData {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) StatusFileName() string {
-	return ""
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) StdoutFileName() string {
-	return ""
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) UnitDir() string {
-	return ""
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) UnredactedStatus() *StatusFileData {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) UpdateBasicStatus(state int, detail string, stdoutSize int64) {
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) UpdateFullStatus(statusFunc func(*StatusFileData)) {
-
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) GetStatusCopy() StatusFileData {
-	return StatusFileData{
-		ExtraData: &CommandExtraData{},
+	w, err := workceptor.New(ctx, mockNetceptor, "/tmp")
+	if err != nil {
+		t.Errorf("Error while creating Workceptor: %v", err)
 	}
+
+	mockBaseWorkUnit.EXPECT().Init(w, "", "", workceptor.FileSystem{}, nil)
+	mockBaseWorkUnit.EXPECT().SetStatusExtraData(gomock.Any())
+	workUnit := workceptor.NewRemoteWorker(mockBaseWorkUnit, w, "", "")
+
+	return workUnit, mockBaseWorkUnit, mockNetceptor, w
 }
-
-func (mock *baseWorkUnitForWorkUnitMock) GetStatusWithoutExtraData() *StatusFileData {
-	return &StatusFileData{}
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) SetStatusExtraData(interface{}) {
-
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) GetStatusLock() *sync.RWMutex {
-	return mock.statusLock
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) GetWorkceptor() *Workceptor {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) SetWorkceptor(*Workceptor) {
-
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) GetContext() context.Context {
-	return nil
-}
-
-func (mock *baseWorkUnitForWorkUnitMock) GetCancel() context.CancelFunc {
-	return nil
-}
-
-// ============================================================
-
-type netceptorForWorkceptorMock struct {
-}
-
-func (mock *netceptorForWorkceptorMock) NodeID() string {
-	return ""
-}
-
-func (mock *netceptorForWorkceptorMock) AddWorkCommand(typeName string, verifySignature bool) error {
-	return nil
-}
-
-func (mock *netceptorForWorkceptorMock) GetClientTLSConfig(name string, expectedHostName string, expectedHostNameType netceptor.ExpectedHostnameType) (*tls.Config, error) {
-	return nil, nil
-}
-
-func (mock *netceptorForWorkceptorMock) GetLogger() *logger.ReceptorLogger {
-	return nil
-}
-
-func (mock *netceptorForWorkceptorMock) DialContext(ctx context.Context, node string, service string, tlscfg *tls.Config) (*netceptor.Conn, error) {
-	return nil, nil
-}
-
-// ============================================================
 
 // Creates a no-op script that can be run during tests.
-// Assumes that /tmp is in PATH which is configured by .vscode/settings.json file.
 func createReceptorPythonWorkerScript() error {
-	// tmpDir := os.TempDir()
 	tmpDir := "/tmp"
 	filename := "receptor-python-worker"
 	absoluteFilename := path.Join(tmpDir, filename)
@@ -171,7 +69,6 @@ func createReceptorPythonWorkerScript() error {
 	}
 	defer f.Close()
 
-	// _, err = f.WriteString("#!/usr/bin/env /bin/sh\necho \"\" > /dev/")
 	_, err = f.WriteString("#!/usr/bin/env /bin/sh\necho \"\" > /dev/null")
 	if err != nil {
 		return fmt.Errorf("Error writing to %s: %v", filename, err)
@@ -186,22 +83,22 @@ func createReceptorPythonWorkerScript() error {
 }
 
 func TestPythonUnitStartRunsToSuccess(t *testing.T) {
-	pw := &pythonUnit{
-		commandUnit: commandUnit{
-			BaseWorkUnitForWorkUnit: &baseWorkUnitForWorkUnitMock{
-				statusLock: &sync.RWMutex{},
-			},
-		},
-		plugin:   "",
-		function: "",
-		config: map[string]any{
-			"test": true,
-		},
-	}
+	_, mockBaseWorkUnit, _, _ := createPythonUnitTestSetup(t)
+	pw := workceptor.NewPythonUnit(mockBaseWorkUnit, "", "", nil)
 
-	// Create an executable script /tmp/receptor-python-worker that echoes
-	// "Hello Receptor" to satisfy the exec.Command() call which hard codes
-	// the command in the pythonUnit.Start() method.
+	var stdoutSize int64 = 0
+	statusLock := &sync.RWMutex{}
+
+	mockBaseWorkUnit.EXPECT().UpdateBasicStatus(workceptor.WorkStatePending, deprecationWarning, stdoutSize)
+	mockBaseWorkUnit.EXPECT().UpdateBasicStatus(workceptor.WorkStatePending, launchPythonRunner, stdoutSize)
+	mockBaseWorkUnit.EXPECT().GetStatusLock().Return(statusLock).Times(2)
+	mockBaseWorkUnit.EXPECT().GetStatusWithoutExtraData().Return(&workceptor.StatusFileData{})
+	mockBaseWorkUnit.EXPECT().GetStatusCopy().Return(workceptor.StatusFileData{
+		ExtraData: &workceptor.CommandExtraData{},
+	})
+	mockBaseWorkUnit.EXPECT().UnitDir()
+	mockBaseWorkUnit.EXPECT().UpdateFullStatus(gomock.Any())
+
 	createReceptorPythonWorkerScript()
 
 	err := pw.Start()
@@ -211,23 +108,25 @@ func TestPythonUnitStartRunsToSuccess(t *testing.T) {
 }
 
 func TestPythonUnitStartFailsOnInvalidConfig(t *testing.T) {
-	pw := &pythonUnit{
-		commandUnit: commandUnit{
-			BaseWorkUnitForWorkUnit: &baseWorkUnitForWorkUnitMock{
-				statusLock: &sync.RWMutex{},
-			},
-		},
-		plugin:   "",
-		function: "",
-		config: map[string]any{
-			"test":      true,
-			"badConfig": math.Inf(1),
-		},
-	}
+	_, mockBaseWorkUnit, _, _ := createPythonUnitTestSetup(t)
 
-	// Create an executable script /tmp/receptor-python-worker that echoes
-	// "Hello Receptor" to satisfy the exec.Command() call which hard codes
-	// the command in the pythonUnit.Start() method.
+	var stdoutSize int64 = 0
+	statusLock := &sync.RWMutex{}
+
+	mockBaseWorkUnit.EXPECT().UpdateBasicStatus(workceptor.WorkStatePending, deprecationWarning, stdoutSize)
+	mockBaseWorkUnit.EXPECT().UpdateBasicStatus(workceptor.WorkStatePending, launchPythonRunner, stdoutSize)
+
+	mockBaseWorkUnit.EXPECT().GetStatusLock().Return(statusLock).Times(2)
+	mockBaseWorkUnit.EXPECT().GetStatusWithoutExtraData().Return(&workceptor.StatusFileData{})
+	mockBaseWorkUnit.EXPECT().GetStatusCopy().Return(workceptor.StatusFileData{
+		ExtraData: &workceptor.CommandExtraData{},
+	})
+
+	badConfig := map[string]any{
+		"badOption": math.Inf(1),
+	}
+	pw := workceptor.NewPythonUnit(mockBaseWorkUnit, "", "", badConfig)
+
 	createReceptorPythonWorkerScript()
 
 	err := pw.Start()
@@ -237,28 +136,32 @@ func TestPythonUnitStartFailsOnInvalidConfig(t *testing.T) {
 }
 
 func TestWorkPythonConfigNewWorkerRunsToSuccess(t *testing.T) {
-	wpc := &WorkPythonCfg{}
-	baseUnit := BaseWorkUnit{}
-	workceptor := &Workceptor{
-		ctx: context.TODO(),
+	_, mockBaseWorkUnitForWorkUnit, mockNetceptorForWorkceptor, _ := createPythonUnitTestSetup(t)
+	mockNetceptorForWorkceptor.EXPECT().NodeID().AnyTimes()
+
+	wpc := &workceptor.WorkPythonCfg{}
+	w, err := workceptor.New(context.TODO(), mockNetceptorForWorkceptor, "")
+	if err != nil {
+		t.Errorf("Error when creating workceptor for test: %v", err)
 	}
-	workUnit := wpc.NewWorker(&baseUnit, workceptor, "", "")
+	workUnit := wpc.NewWorker(mockBaseWorkUnitForWorkUnit, w, "", "")
 	if workUnit == nil {
 		t.Error("Returned WorkUnit was nil")
 	}
 }
 
 func TestWorkPythonConfigRunRunsToSuccess(t *testing.T) {
-	wpc := &WorkPythonCfg{}
-	// baseUnit := BaseWorkUnit{}
-	MainInstance = &Workceptor{
-		ctx:             context.TODO(),
-		workTypesLock:   &sync.RWMutex{},
-		workTypes:       map[string]*workType{},
-		nc:              &netceptorForWorkceptorMock{},
-		activeUnitsLock: &sync.RWMutex{},
+	_, _, mockNetceptorForWorkceptor, _ := createPythonUnitTestSetup(t)
+	mockNetceptorForWorkceptor.EXPECT().NodeID().AnyTimes()
+	mockNetceptorForWorkceptor.EXPECT().AddWorkCommand("", false)
+
+	wpc := &workceptor.WorkPythonCfg{}
+	w, err := workceptor.New(context.TODO(), mockNetceptorForWorkceptor, "")
+	if err != nil {
+		t.Errorf("Error when creating workceptor for test: %v", err)
 	}
-	err := wpc.Run()
+	workceptor.MainInstance = w
+	err = wpc.Run()
 	if err == nil {
 		t.Errorf("Expected deprecation warning but received none.")
 	}

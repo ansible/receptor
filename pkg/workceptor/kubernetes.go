30743b8b2edfb8fce76a06ad1c07f6b4a18cd10d
Andrea Restle-Lay <arestlel@redhat.com>
2025-06-17 12:02:21 -0400

    try to resolve merge conflict

--- pkg/workceptor/kubernetes.go
+++ pkg/workceptor/kubernetes.go
@@ -25,7 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
+	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"

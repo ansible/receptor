0ccf466f54951b3374e8f22128d7efc52ef7c251
Lisa Rashidi-Ranjbar <lranjbar@redhat.com>
2025-07-16 09:58:28 -0700

    Add unit tests for netceptor.Listener

--- pkg/netceptor/conn.go
+++ pkg/netceptor/conn.go
@@ -38,6 +38,12 @@ type QuicConnectionForConn interface {
 	quic.Connection
 }
 
+type QuicListenerForListener interface {
+	Accept(ctx context.Context) (quic.Connection, error)
+	Addr() net.Addr
+	Close() error
+}
+
 type AcceptResult struct {
 	Conn net.Conn
 	Err  error
@@ -47,10 +53,21 @@ type AcceptResult struct {
 type Listener struct {
 	s          *Netceptor
 	pc         PacketConner
-	ql         *quic.Listener
+	ql         QuicListenerForListener
 	AcceptChan chan *AcceptResult
 	DoneChan   chan struct{}
 	doneOnce   *sync.Once
+}
+
+func NewListener(s *Netceptor, pc PacketConner, ql QuicListenerForListener, acceptChan chan *AcceptResult, doneChan chan struct{}, doneOnce *sync.Once) *Listener {
+	return &Listener{
+		s:          s,
+		pc:         pc,
+		ql:         ql,
+		AcceptChan: acceptChan,
+		DoneChan:   doneChan,
+		doneOnce:   doneOnce,
+	}
 }
 
 // Internal implementation of Listen and ListenAndAdvertise.
@@ -292,7 +309,11 @@ func (li *Listener) acceptLoop(ctx context.Context) {
 func (li *Listener) Accept() (net.Conn, error) {
 	select {
 	case ar := <-li.AcceptChan:
-		return ar.Conn, ar.Err
+		if ar == nil {
+			return nil, fmt.Errorf("listener closed")
+		} else {
+			return ar.Conn, ar.Err
+		}
 	case <-li.DoneChan:
 		return nil, fmt.Errorf("listener closed")
 	}

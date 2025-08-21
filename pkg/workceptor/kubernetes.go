40e5a2ebcad2c08a5e5772d4dcc9baf0c97d6891
AaronH88 <ahetheri@redhat.com>
2025-08-21 14:08:12 +0100

    Updated status file on returns, modified the log messages from info to debug, started the delay in retrying a second later

--- pkg/workceptor/kubernetes.go
+++ pkg/workceptor/kubernetes.go
@@ -385,9 +385,8 @@ mainLoop:
 						)
 						*stdoutErr = fmt.Errorf("%s", errMsg)
 						kw.GetWorkceptor().nc.GetLogger().Error("%s", errMsg)
-
-						return
 					}
+					return
 				}
 
 				// Check if the error is not EOF, if error is not EOF retry 5 times if error persists set error and mark the job as failed.
@@ -917,8 +916,8 @@ func (kw *KubeUnit) RunWorkUsingLogger() {
 		streamWait.Done()
 	} else {
 		retryCount := 5
-		prevPodDelay, curPodDelay := 0, 1
-		prevContainerDelay, curContainerDelay := 0, 1
+		prevPodDelay, curPodDelay := 1, 1
+		prevContainerDelay, curContainerDelay := 1, 1
 	podLoop:
 		for {
 			podDetails, kubeErr := kw.KubeAPIWrapperInstance.Get(kw.GetContext(), kw.clientset, podNamespace, podName, metav1.GetOptions{})
@@ -928,14 +927,16 @@ func (kw *KubeUnit) RunWorkUsingLogger() {
 				// Let's try to get the pod again, max 5 times, and decide.
 				retryCount--
 				if retryCount > 0 {
-					kw.GetWorkceptor().nc.GetLogger().Info("Error getting pod while trying to attach stdin: '%s' , continuing try to get pod up to %v more times.", kubeErr, retryCount)
+					kw.GetWorkceptor().nc.GetLogger().Debug("Error getting pod while trying to attach stdin: '%s' , continuing try to get pod up to %v more times.", kubeErr, retryCount)
 
 					time.Sleep(time.Second * time.Duration(curPodDelay))
 					prevPodDelay, curPodDelay = curPodDelay, prevPodDelay+curPodDelay
 
 					continue
 				}
-				kw.GetWorkceptor().nc.GetLogger().Error("Error getting pod %s/%s, after retries exhausted. Error: %s", podNamespace, podName, kubeErr)
+				errMsg := fmt.Sprintf("Error getting pod %s/%s, after retries exhausted. Error: %s", podNamespace, podName, kubeErr)
+				kw.GetWorkceptor().nc.GetLogger().Error(errMsg)
+				kw.UpdateBasicStatus(WorkStateFailed, errMsg, 0)
 
 				return
 			}
@@ -951,7 +952,9 @@ func (kw *KubeUnit) RunWorkUsingLogger() {
 			}
 
 			if !foundContainer {
-				kw.GetWorkceptor().nc.GetLogger().Error("Unable to find the container %s for pod %s. This is unrecoverable. Marking the job as failed and exiting", WorkerContainerName, podName)
+				errMsg := fmt.Sprintf("Unable to find the container %s for pod %s. This is unrecoverable. Marking the job as failed and exiting", WorkerContainerName, podName)
+				kw.GetWorkceptor().nc.GetLogger().Error(errMsg)
+				kw.UpdateBasicStatus(WorkStateFailed, errMsg, 0)
 
 				return
 			}
@@ -959,24 +962,28 @@ func (kw *KubeUnit) RunWorkUsingLogger() {
 			// If container state not running retry or fail job.
 			switch {
 			case containerState.Running != nil:
-				kw.GetWorkceptor().nc.GetLogger().Info("Container in %s pod is running.", podName)
+				kw.GetWorkceptor().nc.GetLogger().Debug("Container in %s pod is running.", podName)
 
 				break podLoop
 			case containerState.Waiting != nil:
 				retryCount--
 				if retryCount > 0 {
-					kw.GetWorkceptor().nc.GetLogger().Info("Container in %s pod is waiting, will retry %v more times.", podName, retryCount)
+					kw.GetWorkceptor().nc.GetLogger().Debug("Container in %s pod is waiting, will retry %v more times.", podName, retryCount)
 
 					time.Sleep(time.Second * time.Duration(curContainerDelay))
 					prevContainerDelay, curContainerDelay = curContainerDelay, prevContainerDelay+curContainerDelay
 
 					continue podLoop
 				}
-				kw.GetWorkceptor().nc.GetLogger().Error("Container in %s pod is not running, retries exhausted", podName)
+				errMsg := fmt.Sprintf("Container in %s pod is not running, retries exhausted", podName)
+				kw.GetWorkceptor().nc.GetLogger().Error(errMsg)
+				kw.UpdateBasicStatus(WorkStateFailed, errMsg, 0)
 
 				return
 			case containerState.Terminated != nil:
-				kw.GetWorkceptor().nc.GetLogger().Error("Container in %s pod has terminated, with exit code: %v, terminated reason: %v and terminated message: %v", podName, containerState.Terminated.ExitCode, containerState.Terminated.Reason, containerState.Terminated.Message)
+				errMsg := fmt.Sprintf("Container in %s pod has terminated, with exit code: %v, terminated reason: %v and terminated message: %v", podName, containerState.Terminated.ExitCode, containerState.Terminated.Reason, containerState.Terminated.Message)
+				kw.GetWorkceptor().nc.GetLogger().Error(errMsg)
+				kw.UpdateBasicStatus(WorkStateFailed, errMsg, 0)
 
 				return
 			default:
@@ -989,7 +996,9 @@ func (kw *KubeUnit) RunWorkUsingLogger() {
 
 					continue podLoop
 				} else {
-					kw.GetWorkceptor().nc.GetLogger().Error("Container in %s pod is not running, retries exhausted", podName)
+					errMsg := fmt.Sprintf("Container in %s pod is not running container state unknown, retries exhausted", podName)
+					kw.GetWorkceptor().nc.GetLogger().Error(errMsg)
+					kw.UpdateBasicStatus(WorkStateFailed, errMsg, 0)
 
 					return
 				}

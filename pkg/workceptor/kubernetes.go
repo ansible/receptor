a56458f7b2baf5a2db0426da2f401407f49b5bcc
matoval <mat24c@gmail.com>
2025-08-21 18:14:30 -0700

    Add test for container switch case

--- pkg/workceptor/kubernetes.go
+++ pkg/workceptor/kubernetes.go
@@ -377,8 +377,8 @@ mainLoop:
 				// Check if the context was canceled and the work state isn't "Succeeded".
 				// If so, set the error and mark the job as failed.
 				if kw.GetContext().Err() == context.Canceled {
-					if kw.Status().State != WorkStateSucceeded ||
-						kw.Status().State != WorkStateFailed ||
+					if kw.Status().State != WorkStateSucceeded &&
+						kw.Status().State != WorkStateFailed &&
 						kw.Status().State != WorkStateCanceled {
 						errMsg := fmt.Sprintf("Context was canceled while reading logs for pod %s/%s. This is unrecoverable. Marking the job as failed and exiting. Error: %s",
 							podNamespace,

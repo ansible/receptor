03cf3c01584a0a45eae9ec6ea5bbddf0bff67255
melissalkelly <mekelly@redhat.com>
2026-01-16 13:09:57 -0500

    Remove whitespace and fix indentation

--- pkg/backends/tcp_test.go
+++ pkg/backends/tcp_test.go
@@ -158,7 +158,7 @@ func TestTCPDialerStart(t *testing.T) {
 			if got == nil {
 				t.Errorf("TCPDialer.Start() returned nil channel")
 			}
-			
+
 			// Wait for connection with 10ms timeout. This is sufficient for loopback
 			// connections (typical datacenter RTT is 0.5ms) while keeping tests fast.
 			select {
@@ -365,9 +365,9 @@ func TestTCPListenerStart(t *testing.T) {
 				} else {
 					conn.Close()
 				}
-				
+
 				// Wait for connection with 10ms timeout. This is sufficient for loopback
-			    // connections (typical datacenter RTT is 0.5ms) while keeping tests fast.
+				// connections (typical datacenter RTT is 0.5ms) while keeping tests fast.
 				select {
 				case <-got:
 					// Connection received

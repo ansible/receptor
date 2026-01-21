4164e9161a90de56d7937a42355b283f22b30523
Dave Mulford <dmulford@redhat.com>
2026-01-21 15:13:56 -0500

    receptorctl: switch to TLS 1.2 minimum version
    Previously, this code disallowed TLS 1.0 and 1.1. However, this may
    have allowed for older SSL versions to be used. This change switches
    to making TLS 1.2 the minimum allowed version.

--- receptorctl/receptorctl/socket_interface.py
+++ receptorctl/receptorctl/socket_interface.py
@@ -125,7 +125,7 @@ class ReceptorControl:
                                 purpose=ssl.Purpose.SERVER_AUTH,
                                 cafile=self._rootcas,
                             )
-                            context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+                            context.minimum_version = ssl.TLSVersion.TLSv1_2
 
                             if self._key and self._cert:
                                 context.load_cert_chain(certfile=self._cert, keyfile=self._key)
